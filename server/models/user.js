2f91308b569b768c498951620d60fec1596f6749
adrian.lux <adorian@akita-itlabo.com>
2018-06-06 15:20:07 +0900

    Build a mongodb api

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2
+    }
+});
+
+module.exports = {User};
---
+++ server/server.js
@@ -0,0 +1,54 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let User =  require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+
+
+/*
+let user = new User({
+    email: 'adrian'
+});
+
+user.save().then((doc) => { 
+    console.log('User saved',doc);
+}, (e) => {
+    console.log('Unable to create user', e);
+
+});
+
+let newTodo = new Todo({
+    text: 'Cook dinner',
+    
+});
+
+newTodo.save().then(() => {
+    console.log('Saved', doc);
+}, (e) => {
+    console.log('Unable to save.');
+});*/
